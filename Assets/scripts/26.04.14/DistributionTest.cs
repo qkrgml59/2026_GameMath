02ce3747386a2a6a1de144938847e70e5f1c6e30
Liesame <aaa01062223008@gmail.com>
2026-04-14 14:59:05 +0900

    [Feat]과제 샘플

---
+++ Assets/scripts/26.04.14/DistributionTest.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class DistributionTest : MonoBehaviour
+{
+   /* int PoissonDistribution(float lambda)
+    {
+        int k = 0;
+        float p = 1f;
+        float L = Mathf.Exp(-lambda);
+        while (p > L)
+        {
+            k++;
+            p *= Random.value;
+        }
+        return k - 1;
+    } */
+
+   
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        /*for (int i = 0; i < 10; i++)
+        {
+            int count = PoissonDistribution(3f);
+            Debug.Log($"Minute {i +1} : {count} event");
+        }*/
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
